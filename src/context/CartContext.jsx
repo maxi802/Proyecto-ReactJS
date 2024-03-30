76aeffca61ac2c3da82ea478e21eda0775734479
maximartinez <bboymaxi17@gmail.com>
2024-03-30 11:31:01 -0300

    Mejorar CSS y Agregar Firebase

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,8 +1,12 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext=createContext();
+
+const carritoInicial = JSON.parse(localStorage.getItem("carrito")) || [] ;
+
 export const CartProvider = ({children}) => {
-    const[carrito,setCarrito]=useState([]);
+    const[carrito,setCarrito]=useState(carritoInicial);
+
     const agregarAlCarrito= (item,cantidad) => {
         const itemAgregado = {...item,cantidad};
         const nuevoCarrito=[...carrito];
@@ -18,6 +22,11 @@ export const CartProvider = ({children}) => {
         setCarrito(nuevoCarrito);
         
     }
+
+    useEffect(() => {
+        localStorage.setItem("carrito",JSON.stringify(carrito))
+    }, [carrito])
+    
 
     const cantidadEnCarrito = () => {
         return carrito.reduce((acc,prod)=>acc+prod.cantidad,0);
